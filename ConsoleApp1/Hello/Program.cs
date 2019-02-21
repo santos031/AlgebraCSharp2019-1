2b0c75aeb8d8a7d226d43dd581fd1aef1c7ef61a
programer <santos.kovacic@gmail.com>
2019-02-21 18:54:58 +0100

    nastavak slijedi

---
+++ ConsoleApp1/Hello/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("Temperatura ledišta vode je: 0 celzijevih stupnjeva");
+            Console.WriteLine("Temperatura vrelišta vode je: 100 celzijevih s");
+
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp1/Zbroj/Program.cs
+++ ConsoleApp1/Zbroj/Program.cs
@@ -10,6 +10,20 @@ namespace Zbroj
     {
         static void Main(string[] args)
         {
+            //deklaracija varijabli
+            int x, y;
+
+            //definicija varijabli x iy
+            x = 5;
+            y = x + 3;
+
+            //ispis u konzolu
+            Console.WriteLine("broj x je:" + x);
+            Console.WriteLine("broj y je:" + y);
+            Console.WriteLine("njihov zbroj je :" + (x+y));
+
+            //ne zatvaraj dok se ne stisne key/
+            Console.ReadKey();
         }
     }
 }
