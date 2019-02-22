6d1fc2487b4dae10330f592e50c148716216d564
Sanja Kovacic <santos.kovacic@gmail.com>
2019-02-22 17:40:47 +0100

    Dijeljenje i umožak

---
+++ ConsoleApp1/zbroj_razlika/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zbroj_razlika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 0;
+            Console.WriteLine("Unesi 1.broj");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesi 2.broj");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zbroj 1. i 2. broja je:{0}", a + b);
+
+            Console.WriteLine("Razlika 1. i 2. broja je:{0}", a - b);
+
+            Console.WriteLine("Umnožak 1. i 2. broja je:{0}", a * b);
+
+            Console.WriteLine("Kvocijent 1. i 2. broja je:{0}", a / b);
+
+            Console.ReadLine();
+        }
+    }
+}
