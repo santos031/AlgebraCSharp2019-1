b46ce98c4078c5d0271fe79615ab9a2d953b4ff2
Sanja Kovacic <santos.kovacic@gmail.com>
2019-02-25 16:49:49 +0100

    uvjetno grananje

---
+++ ConsoleApp1/if_primjer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arg0 = int.Parse(arg[0]);
+            int arg1 = int.Parse(arg[1]);
+
+            Console.WriteLine
+
+        }
+    }
+}
