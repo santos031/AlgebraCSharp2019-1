e44133082f8c28904e3a73116153acaf4332aacb
Sanja Kovacic <santos.kovacic@gmail.com>
2019-02-25 16:33:16 +0100

    odradjena nova vjezba

---
+++ ConsoleApp1/pozitivan_negativan/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pozitivan_negativan
+{
+    class Pozitivan_Negativan
+    {
+        static void Main()
+        {
+            Console.Write("Unesi prirodan broj:");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("Uneseni broj je POZITIVAN.");
+            }
+            else
+            {
+                if (a < 0)
+                {
+                    Console.WriteLine("Uneseni broj je NEGATIVAN.");
+                }
+                else
+                {
+                    Console.WriteLine("Uneseni broj je 0.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/veci_od_10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veci_od_10
+{
+    class Primjer_5_2_1_1_Veci_od_10
+    {
+        static void Main()
+        {
+            Console.Write("Unesi prirodan broj:");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 10)
+            {
+                Console.WriteLine("Uneseni broj je veći od 10.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj NIJE veći od 10.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
