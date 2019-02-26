0f0bf3c73a69dca5c9a8bf1f246224139ee4ca20
Sanja Kovacic <santos.kovacic@gmail.com>
2019-02-26 18:37:30 +0100

    zadaci iz ciklickih petlji

---
+++ ConsoleApp1/zz_5.3.3_suma_100_brojeva/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zz_5._3._3_suma_100_brojeva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nSuma prvih 100.");
+            int suma = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", suma += i);
+            }
+        }
+    }  
+}
