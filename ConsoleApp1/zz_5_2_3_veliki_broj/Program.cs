8af5777b8d693f8dc1a55eb2b9c15ee5479a59e0
Sanja Kovacic <santos.kovacic@gmail.com>
2019-02-26 16:30:20 +0100

    zadaci iz zbirke

---
+++ ConsoleApp1/zz_5_2_3_veliki_broj/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zz_5_2_3_veliki_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 99)
+            {
+                Console.WriteLine("Broj je velik.");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE velik.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/zz_5_2_4_x_znamenkasti_broj/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zz_5_2_4_x_znamenkasti_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj: ");
+            int a = Math.Abs(int.Parse(Console.ReadLine()));
+            if (a < 10)
+            {
+                Console.WriteLine("Uneseni broj je jednoznamenkast.");
+            }
+            else if (a < 100)
+            {
+                Console.WriteLine("Uneseni broj je dvoznamenkast.");
+            }
+            else if (a < 1000)
+            {
+                Console.WriteLine("Uneseni broj je troznamenkast.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj je višeznamenkast.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
