30361f8328290fb471e7503b11a5e0ffbb991796
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-04 18:54:45 +0100

    nizovi u ponedjeljak

---
+++ ConsoleApp1/6.1.1_primjer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._1_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaracija niza
+            int?[] niz = new int?[3];
+            //Inicijalizacija niza 
+            niz[0] = 55;
+            //niz[2] = null;
+            niz[2] = 77;
+            niz[1] = 22;
+
+            try
+            {
+                for (int i = 0; i < niz.Length; i++)
+                {
+                    if (niz[i] == null)
+                    {
+                        throw new Exception("Niz sadrži neinicijaliziranu vrijednost na indexu" + i);
+                    }
+                    Console.WriteLine(i + ": " + niz[i] + niz[5]);
+                    //ispis unazad - ((i+1) + ": " + niz[i]);
+                    //iz knjige - ("{0}. elem. niza: {1}", i, niz[i]);
+                }
+            }
+            catch(IndexOutOfRangeException ioorex)
+            {
+                Console.WriteLine("Pokušali ste dohvatiti nepostojeći član niza" + ioorex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/6.2.1.1_primjer_arraylist/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._1._1_primjer_arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = -1;
+            ArrayList arr = new ArrayList();
+
+            Console.WriteLine("Unesi broj, za kraj unesi 0.");
+
+            while (broj != 0)
+            {
+                broj = int.Parse(Console.ReadLine());
+                arr.Add(broj);
+            }
+
+            //arr.Add(3.14);
+            //System.InvalidCastException, object[double]
+
+            //ispis
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(arr[i]);
+                //ispisuje unesene brojeve do 0
+            }
+            foreach (int brojcic in arr)
+            //ako izostavimo INT ne prolazi u slucaju da unosimo nesto bez metode ToString
+            {
+                Console.Write(" "+brojcic);
+            }
+            Console.ReadKey();
+        }
+    }
+}
