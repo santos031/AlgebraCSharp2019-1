cca5d82d06b05b08119b5a55d84d201e73b7db1c
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-05 18:49:10 +0100

    procedure

---
+++ ConsoleApp1/7.2.2_primjer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._2_primjer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pozdrav("Pero");
+            Pozdrav("Pero", "Perić");
+            Console.ReadKey();
+        }
+
+        static void Pozdrav(string ime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + "!");
+        }
+
+        static void Pozdrav(string ime, string prezime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + " " + prezime + "!");
+        }
+    }
+}
---
+++ ConsoleApp1/7.2_potprogrami/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_potprogrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+
+            Program.HelloWorld(); //ukoliko se nalazim izvan programa upisujem ovo
+
+            Console.WriteLine( HelloWorld("Ola"));
+
+            Console.ReadKey();
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hellloooo");
+        }
+        static String HelloWorld(String poz)
+        {
+            return poz + " hello i tebi";
+        }
+    }
+}
