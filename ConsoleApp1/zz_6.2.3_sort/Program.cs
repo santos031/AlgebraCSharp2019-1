7d4839867377b80f4ab3eb4bffe40156308dc9e0
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-05 18:17:29 +0100

    nizovi i liste zadaci

---
+++ ConsoleApp1/zz_6.2.3_sort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zz_6._2._3_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = -1;
+            ArrayList brojevi = new ArrayList();
+
+            Console.WriteLine("Unos brojeva:");
+
+            while (broj != 0)
+            {
+                broj = int.Parse(Console.ReadLine());
+                brojevi.Add(broj);
+            }
+
+            brojevi.Sort();
+
+            Console.WriteLine("Uneseni brojevi poredani prema veličini:");
+            foreach (int a in brojevi)
+            {
+                Console.WriteLine(" " + a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/zz_6.2.4_znamenke/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zz_6._2._4_znamenke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Unesite prirodan broj");
+            string broj = Console.ReadLine();
+
+            char[]znamenkeArray = broj.ToCharArray();
+            Array.Reverse(znamenkeArray);
+            Console.WriteLine("Ispis znamenki naopako: ");
+            Console.WriteLine(znamenkeArray);
+
+            //ili ovako:
+           //string ulaz_broj = Console.ReadlLine();
+           // int broj = int.Parse(ulaz_broj);
+           //console.writeline("original: {0} Obrnuti {0}, broj, new string(ulaz_broj.reverse().ToArray)")
+           //
+
+            Console.ReadKey();
+        }
+    }
+}
