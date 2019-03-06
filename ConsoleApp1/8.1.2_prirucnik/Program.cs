6d51c3e1ec8528cb40858b0bdaa9310b415b0d85
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-06 18:46:38 +0100

    objekti

--- ConsoleApp1/8.1.2_prirucnik/Program.cs
+++ ConsoleApp1/8.1.2_prirucnik/Program.cs
@@ -10,6 +10,45 @@ namespace _8._1._2_prirucnik
     {
         static void Main(string[] args)
         {
+            //Instanciraj novi objekt klase Ucenik
+            Ucenik uc1 = new Ucenik();
+
+            //Inicijalizacija objekta (postavljanje svojstava objekta)
+            Console.WriteLine("Unesi ime učenika:");
+            uc1.ime = Console.ReadLine();
+
+            Console.WriteLine("Unesi prezime učenika:");
+            uc1.prezime = Console.ReadLine();
+
+            Console.WriteLine("Unesi ocjenu iz Matematike:");
+            uc1.OcjenaizMatematike = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesi ocjenu iz Biologije:");
+            uc1.OcjenaizBiologije = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesi ocjenu iz Engleskog:");
+            uc1.OcjenaizEngleskog = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Prosjek je: " + uc1.Prosjek());
+
+            //instanciraj novi objekt klase Ucenik
+            Ucenik uc2 = new Ucenik("Perica");
+
+            Console.WriteLine("Ime ucenika je " + uc1.ime + " i " + uc2.ime);
+
+            // List<Ucenik> ucenici = new List<Ucenik>(); //moze i ovako
+            List<Ucenik> ucenici = new List<Ucenik>() { uc1, uc2};
+            ucenici.Add(uc1);
+            ucenici.Add(uc2);
+
+            foreach (var item in ucenici)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
--- ConsoleApp1/8.1.2_prirucnik/Ucenik.cs
+++ ConsoleApp1/8.1.2_prirucnik/Ucenik.cs
@@ -1,7 +1,43 @@
-﻿namespace _8._1._2_prirucnik
+﻿
+using System;
+namespace _8._1._2_prirucnik
 {
     internal class Ucenik
     {
         public string ime = "";
+        public DateTime datumNastanka;
+        public string prezime = "";
+        public int OcjenaizMatematike = 0;
+        public int OcjenaizBiologije = 0;
+        public int OcjenaizEngleskog = 0;
+
+        public Ucenik()
+        {
+            this.datumNastanka = DateTime.Now;
+        }
+
+        public Ucenik(string v)
+        {
+            this.ime = v;
+        }
+        public Ucenik(string v, string prez)
+        {
+            this.ime = v;
+            this.prezime = prez;
+        }
+
+        internal double Prosjek()
+        {
+            double prosjek = 0;
+            prosjek = (double)(OcjenaizBiologije + OcjenaizEngleskog + OcjenaizMatematike) / 3;
+
+            return prosjek;
+            //throw new NotImplementedException();
+        }
+
+        public override string ToString() //gaženje bazne metode
+        {
+            return "Moje ime je " + this.ime + " a moj prosjek je: " + this.Prosjek();
+        }
     }
 }
