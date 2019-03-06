5cce1ea67c6beb03a8733cbe025c23d48ddf62c6
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-06 23:24:09 +0100

    objekti diy

---
+++ ConsoleApp1/zz_8.1.1_proizvod/Proizvod.cs
@@ -0,0 +1,22 @@
+﻿namespace zz_8._1._1_proizvod
+{
+    internal class Proizvod
+    {
+        public string Naziv = "";
+        public double Cijena;
+        public double Marza;
+        public double Porez;
+
+        internal double MaloprodajnaCijena()
+        {
+            double mpc = 0;
+            mpc = (double)(Cijena + Marza + Porez);
+
+            return mpc;
+           
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/zz_8.1.2_automobil/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zz_8._1._2_automobil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto a1 = new Auto();
+            
+
+            Console.WriteLine("Unesite marku auta: ");
+            a1.MarkaAutomobila = Console.ReadLine();
+
+            Console.WriteLine("Unesite KS vozila: ");
+            a1.Ks = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite osnovnu cijenu: ");
+            a1.OsnovnaCijena = double.Parse(Console.ReadLine());
+
+    
+            Console.WriteLine("Porez je {0}, a ukupna cijena je {1}.", a1.IznosPoreza(), a1.UkupnaCijena());
+
+            Console.ReadKey();
+        }
+    }
+}
