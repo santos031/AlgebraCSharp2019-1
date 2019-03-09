a91607df3732c28c67a1bd1bdb10bcb7c0e86a9b
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-09 23:48:29 +0100

    zadaca sa lambda funkcijom

---
+++ ConsoleApp1/zadaca_ucenik/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadaca_ucenik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik u1 = new Ucenik("Iva", "Ivić");
+            Ucenik u2 = new Ucenik("Mara", "Marić", "0123456789");
+            Ucenik u3 = new Ucenik("Ana", "Anić", "9876543210", 3500);
+
+            List<Ucenik> listaUcenika = new List<Ucenik>();
+            listaUcenika.Add(u1);
+            listaUcenika.Add(u2);
+            listaUcenika.Add(u3);
+
+            /* ispis liste u foreach petlji
+            foreach (Ucenik ucenik in listaUcenika)
+            {
+                IspisiUcenike(ucenik);
+            }
+            */
+
+            //lambda funkcija ispisa ucenika
+            listaUcenika.ForEach(Ucenik => IspisiUcenike(Ucenik));
+
+            Console.ReadKey();
+        }
+
+        //ispis
+        private static void IspisiUcenike(Ucenik ucenik)
+        {
+            Console.WriteLine(ucenik.ToString());
+            //Console.WriteLine("Učenik: " + ucenik.Ime + " " + ucenik.Prezime + " " + ucenik.Racun + " ("+ ucenik.StvarnoStanjeRacuna() +")");
+        }
+    }
+}
+
---
+++ ConsoleApp1/zadaca_ucenik/Ucenik.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadaca_ucenik
+{
+    class Ucenik
+    {
+        #region Polja
+
+        #endregion
+
+        #region Svojstva
+
+        private string ime = "";
+        public string Ime { get => ime; }
+
+        private string prezime = "";
+        public string Prezime { get => prezime; }
+
+        private string oib;
+        public string Oib { set => oib = value; }
+
+        public string telefon;
+        // public string Telefon { get => telefon; set => telefon = value; }
+
+        private double racun;
+        public double Racun { get => racun; set => racun = value; }
+
+        #endregion
+
+
+        #region Metode
+
+        override
+        public string ToString()
+        {
+            return "Dobar dan " + this.Ime + " " + this.Prezime + "\n Stanje Računa: " + this.StvarnoStanjeRacuna();
+        }
+
+        public int getRandomNumber()
+        {
+            Random random = new Random();
+            int rInt = random.Next(2000, 6000);
+            return rInt;
+        }
+
+        public double StvarnoStanjeRacuna()
+        {
+            return this.Racun - 900;
+        }
+
+        #endregion
+
+
+        #region Konstruktori
+
+        public Ucenik() { }
+
+        public Ucenik(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.racun = getRandomNumber();
+        }
+
+        public Ucenik(string ime, string prezime, string oib)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.Oib = oib;
+            this.racun = getRandomNumber();
+        }
+
+        public Ucenik(string ime, string prezime, string oib, double racun)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.Oib = oib;
+            this.Racun = racun;
+
+            #endregion
+        }
+    }
+}
