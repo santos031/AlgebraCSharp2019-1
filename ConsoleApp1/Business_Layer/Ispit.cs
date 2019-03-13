f47bd9a1d71ddef8b7a71654077a65831620a3f2
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-13 17:38:14 +0100

    događaji

---
+++ ConsoleApp1/Business_Layer/Ispit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+   
+        public class Ispit
+        {
+            private string naziv = "Matematika";
+            private string v;
+
+            public Ispit(string v)
+            {
+                this.Naziv = v;
+            }
+
+            public string Naziv { get => naziv; set => naziv = value; }
+        }
+    }
---
+++ ConsoleApp1/primjer_8.5.1_dogadjaji/Osoba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primjer_8._5._1_dogadjaji
+{
+    public class Osoba
+    {
+        //deklaracija dogadjaja
+        public delegate void NaPromjenuImenaDelegat(object sender, EventArgs e);
+        public event NaPromjenuImenaDelegat NaPromjenuImena;
+
+        //svojstva
+        private string ime;
+        public string Ime
+        {
+            get { return ime; }
+            set
+            {
+                ime = value;
+                if (NaPromjenuImena != null)
+                {
+                    NaPromjenuImena(this, new EventArgs());
+                }
+            }
+        }
+    }
+}
