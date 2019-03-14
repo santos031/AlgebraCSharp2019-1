ea37383be03c2b99ec38758848957eab3c5860cf
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-14 17:38:52 +0100

    datoteke

---
+++ ConsoleApp1/10.1.2_path_file_directory/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _10._1._2_path_file_directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mapa u kojoj se izvršava naš program:");
+            Console.WriteLine(Environment.CurrentDirectory); // Enviroment je static klasa, a CurrentDirectory je svojstvo
+
+            Console.WriteLine("Postoji li file Proba.txt u izvršnoj mapi");
+            
+            //Path klasa
+            string mojaPutanja = Path.Combine(Environment.CurrentDirectory, "Proba.txt"); //Path.Combine spaja klasu sa fileom
+            
+            //File klasa
+            Console.WriteLine(File.Exists(mojaPutanja));
+            
+            //Directory klasa
+            if (!Directory.Exists("Proba")) //Directory je static klasa, a Exists je metoda
+            {
+                //Ako ne postoji folder Proba, kreiram ga
+                Directory.CreateDirectory("Proba");  //CreateDirectory je metoda
+            }
+           
+            //Ispisujem sve foldere na C-u
+            Console.WriteLine("Direktoriji na C:");
+            string[] sDirs = Directory.GetDirectories(@"C:\"); // [] niz
+            foreach (string sDir in sDirs)
+            {
+                Console.WriteLine(sDir);     //ispiši red po red koje si našao
+            }
+
+            //Ispisujem sve fileove na C:
+            Console.WriteLine("Datoteke na C:");
+            string[] sFiles = Directory.GetFiles(@"C:\"); //@ služi kod ispisa pathova kako bi \ bio vidljiv u ispisu
+            foreach (string sFile in sFiles)
+            {
+                Console.WriteLine("\nsFile:");
+                Console.WriteLine(sFile); //ispisuje cijeli path svakog filea
+
+                Console.WriteLine("\nsPath.getfilename: ");
+                Console.WriteLine(Path.GetFileName(sFile));
+
+                Console.WriteLine("\nsPath.getextension: ");
+                Console.WriteLine(Path.GetExtension(sFile));
+
+                Console.WriteLine("\nsFile.getcreationtime: ");
+                Console.WriteLine(File.GetCreationTime(sFile));
+
+                // Console.WriteLine(File.SetCreationTime(sFile, DateTime. .....));
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ConsoleApp1/zz_10.1.2_3_7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace zz_10._1._2_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs1 = new FileStream("NoviRed.txt", FileMode.Create);
+            FileStream fs2 = new FileStream("SaZarezima.txt", FileMode.Create);
+
+            StreamWriter sw1 = new StreamWriter(fs1);
+            StreamWriter sw2 = new StreamWriter(fs2);
+
+           for (int i = 1; i <= 100 ; i++)
+            {
+                if ((i % 3 == 0) || (i % 7 == 0));
+                {
+                    sw1.WriteLine(i);
+                    sw2.Write(i + ", ");
+                }
+            }
+
+            sw1.Flush();
+            sw2.Flush();
+
+            sw1.Close();
+            sw2.Close();
+
+
+            Console.ReadKey();
+        }
+    }
+}
