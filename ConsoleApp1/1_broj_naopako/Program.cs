c3792f3722b1964e5cbd89643559b21724c0666a
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-15 18:17:52 +0100

    ispit

---
+++ ConsoleApp1/1_broj_naopako/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_broj_naopako
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+            Console.WriteLine("Unesite prirodan broj");
+            string broj = Console.ReadLine();
+
+            char[] znamenkeArray = broj.ToCharArray();
+            Array.Reverse(znamenkeArray);
+            Console.WriteLine("Ispis znamenki naopako: ");
+            Console.WriteLine(znamenkeArray);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp1/2_rijeci_abc/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _2_rijeci_abc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> rijeci = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Upišite riječi: ");
+                string rijec = Console.ReadLine();
+
+                if (rijec.ToLower() == "kraj")
+                {
+                    break;
+                }
+                else
+                {
+                    rijeci.Add(rijec);
+                }
+            }
+
+            List<string> rijeciA = (from rijec in rijeci
+                                   where rijec.ToLower().StartsWith("a")
+                                   select rijec).ToList();
+
+            Console.Write("Riječi koje počinju sa slovom A su: ");
+
+            foreach (var rijec in rijeciA)
+            {
+                Console.WriteLine(rijec + ", ");
+            }
+
+            List<string> rijeciB = (from rijec in rijeci
+                                    where rijec.ToLower().StartsWith("b")
+                                    select rijec).ToList();
+
+            Console.Write("Riječi koje počinju sa slovom B su: ");
+
+            foreach (var rijec in rijeciB)
+            {
+                Console.WriteLine(rijec + ", ");
+            }
+
+            List<string> rijeciC = (from rijec in rijeci
+                                    where rijec.ToLower().StartsWith("c")
+                                    select rijec).ToList();
+
+            Console.Write("Riječi koje počinju sa slovom C su: ");
+
+            foreach (var rijec in rijeciC)
+            {
+                Console.WriteLine(rijec + ", ");
+            }
+
+            List<string> rijeciostale = (from rijec in rijeci
+                                    where rijec.ToLower().?????(rijeciA,rijeciB,rijeciC)
+                                    select rijec).ToList();
+
+            Console.Write("Riječi koje počinju sa slovom A su: ");
+
+            foreach (var rijec in rijeciA)
+            {
+                Console.WriteLine(rijec + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
