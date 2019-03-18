533fda84b39404df682996003676d90eb12a1b6e
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-18 18:34:26 +0100

    sql

---
+++ ConsoleApp1/SQL/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿--linijski komentar
+/* blok komentar
+-- CREATE DATABASE upisi
+--GO
+*/
+USE upisi;
+CREATE TABLE #privremena(
+id INT,
+ime NCHAR(50),
+prezime VARCHAR(50),
+mjesto_stanovanja NCHAR(25)
+);
+INSERT INTO [dbo].[polaznici] ([id], [ime], [prezime], [mjesto_stanovanja]) 
+VALUES (0, N'Pero                                              ', 
+N'Peric', 
+N'Osijek                   ');
+SELECT * FROM #privremena;
---
+++ ConsoleApp1/SQL/dbo.polaznici.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[polaznici] ([id], [ime], [prezime], [mjesto_stanovanja]) VALUES (0, N'Pero                                              ', N'Peric', N'Osijek                   ')
+INSERT INTO [dbo].[polaznici] ([id], [ime], [prezime], [mjesto_stanovanja]) VALUES (1, N'Lidija                                            ', N'Lidic', N'Visnjevac                ')
