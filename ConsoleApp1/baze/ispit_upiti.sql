71c363a9762588f36f7659d37a58a9e45218d312
Sanja Kovacic <santos.kovacic@gmail.com>
2019-03-22 17:32:07 +0100

    rjesen ispit

--- ConsoleApp1/baze/ispit_upiti.sql
+++ ConsoleApp1/baze/ispit_upiti.sql
@@ -8,6 +8,20 @@ LEFT JOIN prijatelji p
 ON m.prijatelj_id=p.id
 WHERE m.vratio IS NULL
 
+--4.ZADATAK VIEW
+
+USE Ispit
+GO
+CREATE VIEW prijateljiDuznici
+AS 
+	SELECT p.ime,
+	p.prezime, 
+DATEDIFF(d, m.posudio,GETDATE())as brojDana
+FROM mediji m 
+LEFT JOIN prijatelji p 
+ON m.prijatelj_id=p.id
+WHERE m.vratio IS NULL
+
 --5.ZADATAK
 UPDATE  mediji SET vratio= GETDATE()
 WHERE vratio IS NULL
